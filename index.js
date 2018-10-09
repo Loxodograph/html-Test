14fb53887ed9032ba4cd71c2023e96916fee0473
Loxodograph <43795440+Loxodograph@users.noreply.github.com>
2018-10-09 19:12:40 -0400

    ahhh

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
+const dodger = document.getElementById('dodger')
+var start = false
 function startFunction() {
 	$("#startText").remove();
-    alert ("Hello World!");
+	start = true;
+    ;
+}
+
+function positionToInteger(p) {
+  return parseInt(p.split('px')[0]) || 0
+}
+function moveDodger(e){
+	
+		if(e.which === 37){
+			e.preventDefault();
+    	e.stopPropagation();
+			moveDodgerLeft();
+		}else if(e.which === 39){
+			e.preventDefault();
+    	e.stopPropagation();
+			moveDodgerRight();
+		
+}
+}
+function moveDodgerLeft(){
+	
+	var tug = positionToInteger(dodger.style.left);
+	function step(){
+	dodger.style.left = `${tug + 4}px`;
+}
+if (tug> 0){
+window.requestAnimationFrame(step)
+}
+}
+
+function moveDodgerRight(){
+	var bug = 0;
+	var tug = positionToInteger(dodger.style.left);
+	function step(){
+	tug = `${bug + 4}px`;
+}
+window.requestAnimationFrame(step)
 }
 
 	$(document).ready(function(){
 
 	document.getElementById("startText").addEventListener("click", startFunction);
-
-
+	window.addEventListener("keydown", moveDodger)
+		
+		 
+			
     
 })
+
 
