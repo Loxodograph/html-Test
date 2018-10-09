e904f70cdcfcfbefb41fcc39f49ea49c01c52553
Loxodograph <43795440+Loxodograph@users.noreply.github.com>
2018-10-09 18:23:22 -0400

    Adding some elements

---
+++ index.js
@@ -0,0 +1,13 @@
+function startFunction() {
+	$("#startText").remove();
+    alert ("Hello World!");
+}
+
+	$(document).ready(function(){
+
+	document.getElementById("startText").addEventListener("click", startFunction);
+
+
+    
+})
+
